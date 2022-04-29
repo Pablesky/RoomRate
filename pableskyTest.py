602061736f04e9eed489a4aa79c90b1eb52a4b7a
pablesky <pablesky@live.com>
2022-04-30 01:25:19 +0200

    We have the pictures :)

---
+++ pableskyTest.py
@@ -0,0 +1,21 @@
+from asyncio.base_subprocess import BaseSubprocessTransport
+import requests
+import re as regex
+from bs4 import BeautifulSoup
+
+url = 'https://www.fotocasa.es/es/comprar/vivienda/palma-de-mallorca/aire-acondicionado-calefaccion-terraza-ascensor/163304622/d'
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content,'html.parser')
+with open('test.txt','w') as f:
+    f.write(soup.prettify())
+
+
+list = soup.find_all("img", class_="re-DetailMosaicPhoto")
+lista = []
+for i in list :
+    paraula = regex.search(r'src=\"(.*?)\"', str(i)).group(1)
+    lista.append(paraula)
+
+print(lista)
+
