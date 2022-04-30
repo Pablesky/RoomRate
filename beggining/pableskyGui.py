23073edca297557bf1d9099427b1306ea34fb4ec
pablesky <pablesky@live.com>
2022-04-30 05:38:53 +0200

    gui

---
+++ beggining/pableskyGui.py
@@ -0,0 +1,112 @@
+# image_browser.py
+
+import glob
+import PySimpleGUI as sg
+
+from PIL import Image, ImageTk
+
+from asyncio.base_subprocess import BaseSubprocessTransport
+import requests
+import re as regex
+from bs4 import BeautifulSoup
+import easygui
+import os
+
+def getHTML(url):
+    r = requests.get(url)
+    code = BeautifulSoup(r.content,'html.parser')
+    with open('./data/codeHTML.html', 'w') as f:
+        f.write(code.prettify())
+    return code
+
+def getListLinksPhotos(HTMLcode):
+    HTMLcode.prettify()
+    linesWithImages = HTMLcode.find_all("img", class_="re-DetailMosaicPhoto")
+    imagesLinks = []
+    for linia in linesWithImages :
+        link = regex.search(r'src=\"(.*?)\"', str(linia)).group(1)
+        imagesLinks.append(link)
+    return imagesLinks
+
+def downloadImages(imageLinks):
+    for i, imagen in enumerate(imageLinks):
+        # open image link and save as file
+        response = requests.get(imagen)
+        imagename = './data/FOTO_' + str(i+1) + '.jpg'
+        with open(imagename, 'wb') as file:
+            file.write(response.content)
+
+def cleanDataFolder(): 
+    dir = './data/'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
+
+def parse_folder(path):
+    images = glob.glob(f'{path}/*.jpg') + glob.glob(f'{path}/*.png')
+    return images
+
+def load_image(path, window):
+    try:
+        image = Image.open(path)
+        image.thumbnail((330, 330))
+        photo_img = ImageTk.PhotoImage(image)
+        window["image"].update(data=photo_img)
+    except:
+        print(f"Unable to open {path}!")
+        
+
+def main():
+    column = [sg.Button("Prev"), sg.Image(key="image"), sg.Button("Next")]
+    elements = [
+        [
+            sg.Text("Image File"),
+            sg.Input(size=(25, 1), enable_events=True, key="file"),
+            sg.Button("GO!"),
+        ],
+        column
+    ]
+
+    window = sg.Window("Image Viewer", elements, size=(475, 475))
+    images = []
+    location = 0
+
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if event == "file":
+            images = parse_folder(values["file"])
+            if images:
+                load_image(images[0], window)
+        if event == "Next" and images:
+            if location == len(images) - 1:
+                location = 0
+            else:
+                location += 1
+            load_image(images[location], window)
+        if event == "Prev" and images:
+            if location == 0:
+                location = len(images) - 1
+            else:
+                location -= 1
+            load_image(images[location], window)
+        if event == "GO!":
+            url = values['file']
+            code = getHTML(url)
+            imageLinks = getListLinksPhotos(code)
+            downloadImages(imageLinks)
+
+            contenido = os.listdir('./data/')
+            print(contenido)
+
+            for fichero in contenido:
+                if fichero.endswith('.jpg'):
+                    load_image(images['./data' + fichero], window)
+
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
--- beggining/pableskyTest.py
+++ beggining/pableskyTest.py
@@ -9,9 +9,9 @@ url = 'https://api-us.restb.ai/vision/v2/multipredict'
 payload = {
     # Add your client key
     'client_key': 'b717829c286243060e2429cc405e60bc480b18b2a4fe84b462e47cdf2ff41283',
-    'model_id': 're_roomtype_global_v2,re_features_v3,re_appliances_v2',
+    'model_id': 're_roomtype_global_v2,re_features_v3,re_appliances_v2,re_condition',
     # Add the image URL you want to classify
-    'image_url': 'https://static.inmofactory.com/images/inmofactory/documents/1/88767/19843271/299050732.jpg?rule=web_948x542'
+    'image_url': 'https://static.inmofactory.com/images/inmofactory/documents/1/99202/30824544/482359088.jpg?rule=web_1200x0'
 }
 
 # Make the classify request
@@ -19,4 +19,4 @@ response = requests.get(url, params=payload)
 
 # The response is formatted in JSON
 json_response = response.json()
-print(type(json_response['response']['solutions']['re_features_v3']['detections']))
+print(json_response)
