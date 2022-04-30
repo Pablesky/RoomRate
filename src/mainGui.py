871fcc0cad2693e3f154d12b130139ebe405742c
pablesky <pablesky@live.com>
2022-04-30 07:14:43 +0200

    Working GUI

--- src/mainGui.py
+++ src/mainGui.py
@@ -1,6 +1,5 @@
-# image_browser.py
-
 import glob
+from multiprocessing.sharedctypes import Value
 from turtle import update
 import PySimpleGUI as sg
 
@@ -39,74 +38,63 @@ def cleanDataFolder():
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
 
-
-def parse_folder(path):
-    images = glob.glob(f'{path}/*.jpg') + glob.glob(f'{path}/*.png')
-    return images
-
 def load_image(path, window):
     try:
         image = Image.open(path)
-        image.thumbnail((330, 330))
+        image.thumbnail((min(image.width, 500), min(image.height, 500)))
         photo_img = ImageTk.PhotoImage(image)
         window["image"].update(data=photo_img)
     except:
         print(f"Unable to open {path}!")
-        
+
 def update(contenido, i, window):
     load_image('./data/' + contenido[i], window)
+    window['Prediction'].update(value=contenido[i])
 
 def main():
-    i = 0
-    contenido = []
-    column = [sg.Button("Prev"), sg.Image(key="image"), sg.Button("Next")]
+    indiceFoto = 0
+    ubicacionFotos = []
     elements = [
-        [
-            sg.Text("Image File"),
-            sg.Input(size=(25, 1), enable_events=True, key="file"),
-            sg.Button("GO!"),
-        ],
-        column
+        [sg.Input(size=(25,1), enable_events=True, key='url'), sg.Button("Search!")],
+        [sg.Image(key='image')],
+        [sg.Button('Prev'), sg.Button('Next')],
+        [sg.Text("", size=(0,1), key='Prediction')]
     ]
 
-    window = sg.Window("Image Viewer", elements, size=(475, 475))
-    images = []
-    location = 0
-
+    window = sg.Window("RoomRate", elements, size=(500,500))
+    
     while True:
         event, values = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-        if event == "file":
-            images = parse_folder(values["file"])
-            if images:
-                load_image(images[0], window)
-        if event == "Next":
-            i = i + 1
-            if i == len(contenido):
-                i = len(contenido) - 1
-            print(str(i) + '\n')
-            update(contenido, i, window)
-        if event == "Prev":
-            i = i - 1
-            if i < 0:
-                i = 0
-            print(str(i) + '\n')    
-            update(contenido, i, window)
-        if event == "GO!":
-            url = values['file']
+        if event == 'Search!':
+            url = values['url']
             code = getHTML(url)
             imageLinks = getListLinksPhotos(code)
             downloadImages(imageLinks)
 
-            contenido = os.listdir('./data/')
-            print(contenido)
+            ubicacionFotos = os.listdir('./data/')
+            print(ubicacionFotos)
 
-            load_image('./data/' + contenido[i], window)
+            load_image('./data/' + ubicacionFotos[indiceFoto], window)
 
+            window['Prediction'].update(value=ubicacionFotos[indiceFoto])
 
-    window.close()
+        
+        if event == 'Next':
+            indiceFoto = indiceFoto + 1
+            if indiceFoto == len(ubicacionFotos):
+                indiceFoto = len(ubicacionFotos) - 1
+            print(str(indiceFoto) + '\n')
+            update(ubicacionFotos, indiceFoto, window)
 
+        if event == 'Prev':
+            indiceFoto = indiceFoto - 1
+            if indiceFoto < 0:
+                indiceFoto = 0
+            print(str(indiceFoto) + '\n')
+            update(ubicacionFotos, indiceFoto, window)
+        
+        if event == sg.WIN_CLOSED:
+            break    
 
 if __name__ == "__main__":
     main()
